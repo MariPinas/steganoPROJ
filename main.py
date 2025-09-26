c9090750e39654a07e033d05c0c7810d2a5df682
FlaviaGoes <flav.goes@outlook.com>
2025-09-25 21:23:46 -0300

    :sparkles: added menu

--- main.py
+++ main.py
@@ -1,32 +1,31 @@
 from hide import *
 from reveal import *
 
-def Steganography():
+def menu():
     while True:
         print("\n" + "="*25)
-        print("    Esteganografia em LSB")
+        print("   Esteganografia")
         print("="*25)
-        print("1. Codificar mensagem em imagem")
-        print("2. Decodificar mensagem de imagem")
+        print("1. Esconder mensagem em imagem")
+        print("2. Revelar mensagem da imagem")
         print("3. Sair")
         user_option_str = input("Insira uma opção: ")
 
-        if user_option_str == '1':
-            print("\n--- Modo de Codificação ---")
-            encode_text()
-        elif user_option_str == '2':
-            print("\n--- Modo de Decodificação ---")
-            decoded_message = decode_text()
-            print("\n--- Mensagem Decodificada ---")
-            print(decoded_message)
-            print("-----------------------------")
-        elif user_option_str == '3':
-            print("Saindo do programa.")
-            break
-        else:
-            print("Opção inválida, por favor insira um número de 1 a 3.")
-
+        match user_option_str:
+            case '1':
+                print("\n --- Esconder Mensagem")
+            case '2':
+                print("\n --- Revelar Mensagem")
+                mensagem_revelada = 'Mensagem Revelada'
+                print("Mensagem revelada: "), mensagem_revelada
+                print("-----------------------------")
+            case '3':
+                print('Finalizando Esteganografia')
+                break
+            case _:
+                print('Opção inválida')
+                
         input("\nPressione Enter para voltar ao menu...")
 
 if __name__ == '__main__':
-    Steganography()
+    menu()
