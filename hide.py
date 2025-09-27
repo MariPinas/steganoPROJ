3e0d222e38a704510f9e2d6130d06360197db072
Andriel <andriel.h@aluno.ifsp.edu.br>
2025-09-27 18:35:38 -0300

    :sparkles: edit hide function

--- hide.py
+++ hide.py
@@ -1,23 +1,33 @@
 import cv2
+from rich import print
 
 def hide_message():
-    message = input("Insira uma mensagem: ")
+    message = input("Insira uma mensagem: ") + "#####"
     file_name = input("Insira o nome do arquivo da imagem: ")
-
+    
     read_image = cv2.imread(file_name)
-    image_resize = cv2.resize(read_image, (512, 512))
+
+    if read_image is None:
+        print("[red]Imagem nao encontrada![/red]")
+        return
+    
+    #image_resize = cv2.resize(read_image, (512, 512)) // comentei pra deixar a img do tamanho real que ela é
     binary_message = ''.join([format(ord(char), '08b') for char in message])
     lenght_message = len(binary_message)
 
-    b, g, r = cv2.split(image_resize)
+    b, g, r = cv2.split(read_image)
     blue_layer = b.copy()
 
     counter = 0
     height, width = blue_layer.shape
 
+    if lenght_message > height * width:
+        print("[red]Mensagem muito grande para essa imagem![/red]")
+        return
+
     for i in range(height):
         for j in range(width):
-            if(counter < lenght_message):
+            if counter < lenght_message:
                 pixel = blue_layer[i, j]
                 bit_input = int(binary_message[counter])
                 pixel &= 0b11111110
@@ -27,3 +37,4 @@ def hide_message():
     
     hide_image = cv2.merge((blue_layer, g, r))
     cv2.imwrite('secret_image.png', hide_image)
+    print("[green]Mensagem escondida em 'secret_image.png' com sucesso![/green]")
